bbc149720aab1cc429c2c86e55f36ec7ab049db3
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2023-07-29 03:09:21 +0000

    Update App.js and ProductItem.js

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,54 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ProductItem = ({ products, onItemClick }) => {
-  const handleItemClick = (product) => {
-    // Call the onItemClick function when an item is clicked
+const ProductItem = ({ product, onItemClick }) => {
+  const handleItemClick = () => {
     onItemClick(product);
   };
 
   return (
     <div>
       <ul>
-        {products &&
-          products.map((product, index) => (
-            <li
-              key={index}
-              style={{ listStyleType: "none" }}
-              onClick={() => handleItemClick(product)} // Call the handler on item click
-            >
-              <h3>{product.name}</h3>
-              <span>
-                <img
-                  src={product.image_front}
-                  alt={product.name}
-                  style={{ width: "200px", height: "220px" }}
-                />
-                <img
-                  src={product.image_back}
-                  alt={product.name}
-                  style={{ width: "200px", height: "220px" }}
-                />
-              </span>
-              <p>Price: ${product.price}</p>
-            </li>
-          ))}
-        <hr />
+        <li style={{ listStyleType: "none" }} onClick={handleItemClick}>
+          <h3>{product.name}</h3>
+          <span>
+            <img
+              src={product.image_front}
+              alt={product.name}
+              style={{ width: "200px", height: "220px" }}
+            />
+            <img
+              src={product.image_back}
+              alt={product.name}
+              style={{ width: "200px", height: "220px" }}
+            />
+          </span>
+          <p>Price: ${product.price}</p>
+        </li>
       </ul>
-      <p>additional content</p>
+      <hr />
     </div>
   );
 };
 
 // Add prop-types definition
 ProductItem.propTypes = {
-  products: PropTypes.arrayOf({
+  product: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     image_front: PropTypes.string.isRequired,
     image_back: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  onItemClick: PropTypes.func.isRequired
 };
 
 export default ProductItem;
