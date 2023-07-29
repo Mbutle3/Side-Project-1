75a36c8bfcc4ff965b5e347a3a521594f517c7fe
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2023-07-29 04:37:27 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "../src/App";
-
+import Header from "../src/components/common/Header";
+import Footer from "../src/components/common/Footer";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <Header />
     <App />
+    <Footer />
   </StrictMode>
 );
